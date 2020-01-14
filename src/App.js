7662d58d6e41bfb20e0a335d3781fd36db3edb4c
joe-cooper <33054985+cooper-joe@users.noreply.github.com>
2020-01-14 13:10:50 +0100

    feat: dashboard item examples

--- src/App.js
+++ src/App.js
@@ -2,25 +2,89 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import {CssReset, CssVariables, Button, Card} from '@dhis2/ui-core';
+import {ThreeDots, SpeechBubble} from './assets/icons.js';
+import {DevExample} from './assets/DevExample.js';
+
+const DashboardItem = props => (
+  <Card>
+    <ItemWrap>
+      <MetaWrap>
+        <Title>{props.title}</Title>
+        <ItemActions/>
+      </MetaWrap>
+      <ItemContent>{props.children}</ItemContent>
+  </ItemWrap>
+  </Card>
+)
+
+const ItemWrap = props => (
+  <div class="item-wrap">
+    {props.children}
+  </div>
+)
+
+const MetaWrap = props => (
+  <div class="item-meta-wrap">
+    {props.children}
+  </div>
+)
+
+const Title = props => (
+  <p class='item-title'>{props.children}</p>
+)
+
+const ItemActions = props => (
+  <div class='item-actions-wrap'>
+    <Button small secondary><SpeechBubble/></Button>
+    <Button small secondary><ThreeDots/></Button>
+  </div>
+)
+
+const ItemContent = props => (
+  <div class='item-content-wrap'>
+    {props.children}
+  </div>
+)
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <CssReset/>
+    <CssVariables colors spacers />
+
+    <DevExample title="Short title">
+      <DashboardItem title="Malaria Cases 2019">
+        <DemoItem/>
+      </DashboardItem>
+    </DevExample>
+
+    <DevExample title="Stacked long title">
+      <DashboardItem title="Immunization: Penta 3 Coverage, Measles, BCG, Yellow Fever across all Chiefdoms last six-months in user organisation units">
+        <DemoItem/>
+      </DashboardItem>
+    </DevExample>
+
+
+    <DevExample title="Stacked long title in a narrow item">
+      <NarrowWrap>
+      <DashboardItem title="Immunization: Penta 3 Coverage, Measles, BCG, Yellow Fever across all Chiefdoms last six-months in user organisation units">
+        <DemoItem/>
+      </DashboardItem>
+      </NarrowWrap>
+    </DevExample>
+
+    </>
   );
 }
+
+// dev only
+const DemoItem = () => (
+  <div class="demo-item">Map/Chart/Table</div>
+)
+
+const NarrowWrap = props => (
+  <div class="narrow-wrap">{props.children}</div>
+)
 
 export default App;
