00d76fe39416952f415b217d3e77b7af5a5f87c4
Joe Cooper <33054985+cooper-joe@users.noreply.github.com>
2020-04-27 13:30:11 +0200

    feat: separate route files (#13)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch } from "react-router-dom";
 import { CssReset, CssVariables } from "@dhis2/ui-core";
-import SpecComment from "./components/comment.js";
-import SpecRichText from "./components/richText.js";
-import SpecTag from "./components/tag.js";
-import SpecDataTable from "./components/data-table.js";
-import SpecColorPicker from "./components/color-picker.js";
+import { appRoutes, componentRoutes } from "./AppRoutes.js";
 import "./App.css";
 
 export default function App() {
@@ -14,73 +10,14 @@ export default function App() {
       <CssReset />
       <CssVariables colors spacers />
       <Router>
-        <div>
-          {/* A <Switch> looks through its children <Route>s and
+        {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
-          <Switch>
-            <Route path="/comment">
-              <BackLink />
-              <SpecComment />
-            </Route>
-            <Route path="/rich-text">
-              <BackLink />
-              <SpecRichText />
-            </Route>
-            <Route path="/tag">
-              <BackLink />
-              <SpecTag />
-            </Route>
-            <Route path="/data-table">
-              <BackLink />
-              <SpecDataTable />
-            </Route>
-            <Route path="/color-picker">
-              <BackLink />
-              <SpecColorPicker />
-            </Route>
-            <Route path="/">
-              <Home />
-            </Route>
-          </Switch>
-        </div>
+        <Switch>
+          {componentRoutes}
+
+          {appRoutes}
+        </Switch>
       </Router>
     </>
   );
 }
-
-function Home() {
-  return (
-    <nav>
-      Available Component Specs
-      <ul>
-        <li>
-          <Link to="/comment">Comment</Link>
-        </li>
-        <li>
-          <Link to="/rich-text">Rich Text</Link>
-        </li>
-        <li>
-          <Link to="/tag">Tag</Link>
-        </li>
-        <li>
-          <Link to="/data-table">Data Table</Link>
-        </li>
-        <li>
-          <Link to="/color-picker">Color Picker</Link>
-        </li>
-      </ul>
-      Other examples/mockups
-      <ul>
-        <li>
-          <Link to="/dv-start-screen">Data Visualizer: Start Screen</Link>
-        </li>
-      </ul>
-    </nav>
-  );
-}
-
-const BackLink = () => (
-  <div class="back-link">
-    <Link to="/">Back to component list</Link>
-  </div>
-);
