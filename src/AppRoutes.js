d346c861085ba166708a9db4b32416d4954491b1
joe-cooper <33054985+cooper-joe@users.noreply.github.com>
2020-04-27 12:59:59 +0200

    app routes

---
+++ src/AppRoutes.js
@@ -0,0 +1,77 @@
+import React from "react";
+import { Route, Link } from "react-router-dom";
+
+import SpecComment from "./components/comment.js";
+import SpecRichText from "./components/richText.js";
+import SpecTag from "./components/tag.js";
+import SpecDataTable from "./components/data-table.js";
+import SpecColorPicker from "./components/color-picker.js";
+import { StateExp } from "./components/state.js";
+
+const BackLink = () => (
+  <div class="back-link">
+    <Link to="/">Back to component list</Link>
+  </div>
+);
+
+export const appRoutes = [
+  <Route path="/">
+    <Home />
+  </Route>
+];
+
+function buildRoute(path, Component) {
+  return (
+    <Route path={path}>
+      <BackLink />
+      <Component />
+    </Route>
+  );
+}
+
+const rawComponents = [
+  { url: "/comment", component: SpecComment },
+  { url: "/rich-text", component: SpecRichText },
+  { url: "/tag", component: SpecTag },
+  { url: "/data-table", component: SpecDataTable },
+  { url: "/color-picker", component: SpecColorPicker },
+  { url: "/state", component: StateExp }
+];
+
+export const componentRoutes = rawComponents.map(object =>
+  buildRoute(object.url, object.component)
+);
+
+function Home() {
+  return (
+    <nav>
+      Available Component Specs // turn this dynamic
+      <ul>
+        <li>
+          <Link to="/comment">Comment</Link>
+        </li>
+        <li>
+          <Link to="/rich-text">Rich Text</Link>
+        </li>
+        <li>
+          <Link to="/tag">Tag</Link>
+        </li>
+        <li>
+          <Link to="/data-table">Data Table</Link>
+        </li>
+        <li>
+          <Link to="/color-picker">Color Picker</Link>
+        </li>
+        <li>
+          <Link to="/state">State</Link>
+        </li>
+      </ul>
+      Other examples/mockups
+      <ul>
+        <li>
+          <Link to="/dv-start-screen">Data Visualizer: Start Screen</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
