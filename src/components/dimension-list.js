047477655602709c746b1986094436015c8a47f9
Joe Cooper <joe@dhis2.org>
2020-12-08 13:50:50 +0100

    feat: dim-list, drag only and locked items

--- src/components/dimension-list.js
+++ src/components/dimension-list.js
@@ -14,10 +14,18 @@ const DimensionItem = (props) => (
   ${props.picked ? ' picked' : ''}
   ${props.disabled ? ' disabled' : ''}
   ${props.recommended ? ' recommended' : ''}
+  ${props.dragOnly ? ' drag-only' : ''}
   `}
   >
     <div class="di-icon">{props.icon}</div>
     <div class="di-label">{props.children}</div>
+    {props.locked ? (
+      <div class="di-lock">
+        <IconLock />
+      </div>
+    ) : (
+      ''
+    )}
     <div class="di-menu-trigger">
       <IconMore />
     </div>
@@ -53,6 +61,20 @@ export default function specDimensionList() {
           </DimensionItem>
           <DimensionItem icon={<DemoIcon1 />} disabled>
             Disabled item
+          </DimensionItem>
+          <DimensionItem picked locked icon={<DemoIconData />}>
+            Data
+          </DimensionItem>
+        </DimensionSidebar>
+      </DevExample>
+
+      <DevExample
+        title="Drag only items"
+        notes="Some dimension items cannot be clicked to open a modal. There are only two actions: click the menu trigger, or drag the item. In this case, the whole item is draggable. (Example: Assigned Categories in Data Visualizer)."
+      >
+        <DimensionSidebar>
+          <DimensionItem dragOnly icon={<DemoIconAC />}>
+            Assigned Categories
           </DimensionItem>
         </DimensionSidebar>
       </DevExample>
@@ -143,6 +165,35 @@ const IconMore = (props) => (
       d="m3 7c.55228475 0 1 .44771525 1 1s-.44771525 1-1 1-1-.44771525-1-1 .44771525-1 1-1zm5 0c.55228475 0 1 .44771525 1 1s-.44771525 1-1 1-1-.44771525-1-1 .44771525-1 1-1zm5 0c.5522847 0 1 .44771525 1 1s-.4477153 1-1 1-1-.44771525-1-1 .4477153-1 1-1z"
       fill="inherit"
       fill-rule="evenodd"
+    />
+  </svg>
+);
+
+const DemoIconAC = () => (
+  <svg
+    height="16"
+    viewBox="0 0 16 16"
+    width="16"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="m10 10-1 1h-2l-1-1zm3-3-1 1h-8l-1-1zm2-3-1 1h-12l-1-1z"
+      fill="inherit"
+      fill-rule="evenodd"
+    />
+  </svg>
+);
+
+const IconLock = () => (
+  <svg
+    height="16"
+    viewBox="0 0 16 16"
+    width="16"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="m8 1c1.65685425 0 3 1.34314575 3 3v2h1c.5522847 0 1 .50367966 1 1.125v6.75c0 .6213203-.4477153 1.125-1 1.125h-8c-.55228475 0-1-.5036797-1-1.125v-6.75c0-.62132034.44771525-1.125 1-1.125h1v-2c0-1.65685425 1.34314575-3 3-3zm4 6h-8v7h8zm-4 2c.55228475 0 1 .44771525 1 1 0 .5522847-.44771525 1-1 1s-1-.4477153-1-1c0-.55228475.44771525-1 1-1zm0-7c-1.0543618 0-1.91816512.81587779-1.99451426 1.85073766l-.00548574.14926234v2h4v-2c0-1.1045695-.8954305-2-2-2z"
+      fill="inherit"
     />
   </svg>
 );
