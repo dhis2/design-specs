94279dcf84e113f7b36c81b5942ddb84813a6a6d
Joe Cooper <joe@dhis2.org>
2021-01-07 13:42:19 +0100

    fix(headerbar-spec): adjust badge color

--- src/components/headerbar.js
+++ src/components/headerbar.js
@@ -13,7 +13,6 @@ const Headerbar = (props) => (
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 200 182"
           class="logo"
-          data-test="dhis2-uicore-logoiconwhite"
         >
           <defs></defs>
           <path
